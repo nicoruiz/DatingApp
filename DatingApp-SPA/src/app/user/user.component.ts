12c8017f6acdde38a9cc25ace22cb114f425f8a8
Miguel Nicolas Ruiz <Miguel.Nicolas.Ruiz@ey.com>
2019-10-23 11:45:39 -0300

    Initial commit

---
+++ DatingApp-SPA/src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5000/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log('error: ' + error);
+    });
+  }
+
+}
---
+++ DatingApp/DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly ILogger<UsersController> _logger;
+        private readonly DataContext _context;
+
+
+        public UsersController(ILogger<UsersController> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            return Ok(user);
+        }
+    }
+}
